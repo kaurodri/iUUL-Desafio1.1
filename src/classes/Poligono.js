c6cf46b6d3066c453e5807781bcfc48a1b49ee95
HieA1lpha <engkauanrodrigues@gmail.com>
2024-10-28 17:00:28 -0300

    2024-10-28

--- src/classes/Poligono.js
+++ src/classes/Poligono.js
@@ -1,7 +1,36 @@
-import Vertice from './Vertice.js';
-
 class Poligono {
+    #vertices;
 
+    constructor(vertices) {
+        if (vertices.length < 3) {
+            throw new Error("O poligono precisa de pelo menos 3 vértices.");
+        }
+        this.#vertices = vertices;
+    }
+
+    get perimetro() {
+        let perimetro = 0;
+        for (let i = 0; i < this.#vertices.length; i++) {
+            const verticeAtual = this.#vertices[i];
+            const proximoVertice = this.#vertices[(i + 1) % this.#vertices.length];
+            perimetro += verticeAtual.distancia(proximoVertice);
+        }
+        return perimetro;
+    }
+
+    get qtdVertices() {
+        return this.#vertices.length;
+    }
+
+    addVertice(v) {
+        for (let vertice of this.#vertices) {
+            if (vertice.equals(v)) {
+                return false;
+            }
+        }
+        this.#vertices.push(v);
+        return true;
+    }
 }
 
 export default Poligono;
---
+++ src/questoes/questao3.js
@@ -0,0 +1,38 @@
+import { Vertice, Poligono } from '../classes/index.js';
+import promptSync from 'prompt-sync';
+
+const entrada = promptSync({ sigint: true });
+
+const criar = function (num) {
+    const x = entrada(`[ ${num} Vértice ] coordenada x: `);
+    const y = entrada(`[ ${num} Vértice ] coordenada y: `);
+    return new Vertice(Number(x), Number(y));
+};
+
+const vertices = [];
+
+console.log("Adicione os 3 primeiros vértices do polígono:");
+
+vertices.push( criar('1º') );
+vertices.push( criar('2º') );
+vertices.push( criar('3º') );
+
+const poligono = new Poligono(vertices);
+
+let adicionandoVertices = true;
+while (adicionandoVertices) {
+    const adicionar = entrada("Adicionar outro vértice? [ s / n ]: ").toLowerCase();
+    if (adicionar === 's') {
+        const novoVertice = criar('Novo');
+
+        let verificar = !poligono.addVertice(novoVertice) ? 'Este vértice já existe no polígono.' : 'Vértice adicionado com sucesso.';
+        console.log(verificar);
+    } else {
+        adicionandoVertices = false;
+    }
+}
+    
+let conteudo = '';
+conteudo += `O polígono tem ${poligono.qtdVertices} vértices.\n`;
+conteudo += `O perímetro do polígono é: ${poligono.perimetro.toFixed(2)}`;
+console.log(conteudo);
